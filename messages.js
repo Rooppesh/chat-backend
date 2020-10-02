baa9e7b02413ff7bdbd7cba913386a0fe8be38c1
Rooppesh <rooppesh98@gmail.com>
2020-10-02 22:13:50 +0530

    Added Pusher

--- messages.js
+++ messages.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const chatSchema = mongoose.Schema({
+  roomId: Number,
   message: String,
   name: String,
   timestamp: String,
   received: Boolean,
 });
 
-export default mongoose.model("messageContent", chatSchema);
+export default mongoose.model("messagecontents", chatSchema);
--- server.js
+++ server.js
@@ -2,27 +2,61 @@
 import express from "express";
 import mongoose from "mongoose";
 import Message from "./messages.js";
+import Pusher from "pusher";
+import Constants from "./constants.js";
 
 // App Config
 const app = express();
 const port = process.env.PORT || 9000;
 
+var pusher = new Pusher({
+  appId: Constants.PUSHER_APP_ID,
+  key: Constants.PUSHER_KEY,
+  secret: Constants.PUSHER_SECRET,
+  cluster: "ap2",
+  encrypted: true,
+});
+
 // Middleware
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 // DB Config
-const connection_url =
-  "mongodb+srv://admin:UAtyR8tX5CDUGlDE@cluster0.youzg.mongodb.net/chatdb?retryWrites=true&w=majority";
+const connection_url = Constants.MONGO_CONNECTION_STRING;
 mongoose.connect(connection_url, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// ????
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log(`DB Connected`);
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log(`DB Updated : `, change);
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        roomId: messageDetails.roomId,
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+      });
+    } else {
+      console.log("Error Triggering Pusher!");
+    }
+  });
+});
 
 // Api Routes
-app.get("/", (req, res) => res.status(200).send("Hello World"));
+app.get("/", (req, res) => res.status(200).send("Server is Up & Running.. :)"));
 
 app.post("/v1/message/create", (req, res) => {
   const messageBody = req.body;
@@ -33,6 +67,19 @@ app.post("/v1/message/create", (req, res) => {
       res.status(201).send(data);
     }
   });
+});
+
+app.get("/v1/message/sync", (req, res) => {
+  Message.find({})
+    .where("roomId")
+    .equals(req.query.roomId)
+    .exec(function (err, data) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    });
 });
 
 // Listener
