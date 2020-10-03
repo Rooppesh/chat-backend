20c9fc5ad129f902d32877acd955c3fd6685d800
Rooppesh <rooppesh98@gmail.com>
2020-10-03 22:51:09 +0530

    Added Heroku

---
+++ constants.js
@@ -0,0 +1,8 @@
+export default {
+  MONGO_CONNECTION_STRING:
+    "mongodb+srv://admin:UAtyR8tX5CDUGlDE@cluster0.youzg.mongodb.net/chatdb?retryWrites=true&w=majority",
+  PUSHER_APP_ID: "1083855",
+  PUSHER_KEY: "225d7f20e06e9a51b940",
+  PUSHER_SECRET: "97b502940c28a457c4ee",
+  PUSHER_CLUSTER: "ap2",
+};
